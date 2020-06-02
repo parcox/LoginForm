cbe9dca44122d55dffcd4f409657f350a3dfff06
Fitri Wibowo <parcoxs@gmail.com>
2020-06-02 07:11:36 +0700

    Initial commit

---
+++ app/src/main/java/id/web/owob/loginform/data/LoginDataSource.java
@@ -0,0 +1,29 @@
+package id.web.owob.loginform.data;
+
+import id.web.owob.loginform.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TO DO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TO DO: revoke authentication
+    }
+}
